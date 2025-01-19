168f2cdd37e0b9099691e8f8f1b806133912d275
CatHood0 <santiagowmar@gmail.com>
2025-01-19 04:18:08 -0400

    Export QueryDeltaParams

--- lib/dart_quill_delta_simplify.dart
+++ lib/dart_quill_delta_simplify.dart
@@ -2,6 +2,7 @@ library dart_quill_delta_simplify;
 
 export 'src/build_result.dart';
 export 'src/query_delta.dart';
+export 'src/query_delta_params.dart';
 export 'src/util/enums.dart';
 export 'extensions.dart';
 export 'conditions.dart';
