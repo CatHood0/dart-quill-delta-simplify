0ebfa28c98c731331d3b1dc319c21844281c5867
CatHood0 <santiagowmar@gmail.com>
2025-01-18 20:01:25 -0400

    Chore(doc): improved doc comments in OverlapDeltaRangeListExtension

--- lib/src/extensions/list_ext.dart
+++ lib/src/extensions/list_ext.dart
@@ -7,29 +7,7 @@ import '../../delta_ranges.dart';
 extension OverlapDeltaRangeListExtension on List<DeltaRange> {
   /// Checks if the provided [range] overlaps with any `DeltaRange` in the list.
   ///
-  /// This method iterates over the list of `DeltaRange` and uses the
-  /// `checkOverlapOfRanges` method to determine if the given [range] overlaps
-  /// with any element in the list. If an overlap is found, the method returns
-  /// `true`. If no overlap is found or if the list is empty, it returns `false`.
-  ///
-  /// - [range]: The `DeltaRange` to check for overlaps. If `null`, the method
-  ///   returns `false`.
-  ///
-  /// Returns `true` if an overlap is detected; otherwise, returns `false`.
-  ///
-  /// ## Examples
-  ///
-  /// ```dart
-  /// final ignoreTheseRanges = [
-  ///   DeltaRange(startOffset: 0, endOffset: 50),
-  ///   DeltaRange(startOffset: 60, endOffset: 100)
-  /// ];
-  ///
-  /// final newRange = DeltaRange(startOffset: 40, endOffset: 70);
-  ///
-  /// // Check if newRange overlaps with any existing range
-  /// final result = ignoreTheseRanges.ignoreOverlap(newRange); // true
-  /// ```
+  /// ## Example
   ///
   /// ```dart
   /// final ignoreTheseRanges = [
