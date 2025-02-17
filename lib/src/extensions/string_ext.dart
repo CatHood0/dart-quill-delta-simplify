6398ed70e29e24894095135a362976e6fcb935f6
CatHood0 <santiagowmar@gmail.com>
2025-02-17 02:10:05 -0400

    Chore: removed missed old unnecessary files and method in string ext

--- lib/src/extensions/string_ext.dart
+++ lib/src/extensions/string_ext.dart
@@ -1,18 +1,6 @@
 final RegExp _newLinesRegexp = RegExp(r'^\n+$');
 
 extension StringExt on String {
-  String? substringOrNull(int start, int end,
-      [bool returnEmptyString = false]) {
-    return start > 0 &&
-            start < length &&
-            (end >= start && end > 0) &&
-            end < length
-        ? substring(start, end)
-        : returnEmptyString
-            ? ''
-            : null;
-  }
-
   bool startsAndEndsWith(String pattern) {
     return startsWith(pattern) && endsWith(pattern) ||
         length == 1 && endsWith(pattern) ||
